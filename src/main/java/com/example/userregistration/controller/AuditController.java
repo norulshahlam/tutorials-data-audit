b332d1b3079a66435808a570993919327cabbc04
Norulshahlam <EXT.NBINMOHSEN@cma-cgm.com>
2023-11-20 16:04:17 +0800

    updated

---
+++ src/main/java/com/example/userregistration/controller/AuditController.java
@@ -0,0 +1,52 @@
+package com.example.userregistration.controller;
+
+import com.example.userregistration.entity.BookingEntity;
+import com.example.userregistration.entity.ContactEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.javers.core.Changes;
+import org.javers.core.Javers;
+import org.javers.repository.jql.QueryBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/audit")
+@Slf4j
+public class AuditController {
+
+    private final Javers javers;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public AuditController(Javers javers) {
+        this.javers = javers;
+    }
+
+    @GetMapping("/booking")
+    public ResponseEntity<String> getBookingEntityChanges() {
+        QueryBuilder jqlQuery = QueryBuilder.byClass(BookingEntity.class);
+        Changes changes = javers.findChanges(jqlQuery.build());
+        return ResponseEntity.ok().body("<pre>" + changes.prettyPrint() + "</pre>");
+    }
+
+    @GetMapping("/contact")
+    public ResponseEntity<String> getContactEntityChanges() {
+        QueryBuilder jqlQuery = QueryBuilder.byClass(ContactEntity.class);
+        Changes changes = javers.findChanges(jqlQuery.build());
+        return ResponseEntity.ok().body("<pre>" + changes.prettyPrint() + "</pre>");
+    }
+
+    @GetMapping("/contact/{id}")
+    public String getContactEntityChanges(@PathVariable String id) {
+        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(id, ContactEntity.class)
+                .withNewObjectChanges();
+
+        Changes changes = javers.findChanges(jqlQuery.build());
+
+        return "<pre>" + changes.prettyPrint() + "</pre>";
+    }
+}
