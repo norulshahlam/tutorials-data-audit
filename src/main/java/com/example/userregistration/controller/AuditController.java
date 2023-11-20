01f7d0a1063195fcb21a522fda9804b01f5e01cb
Norulshahlam <EXT.NBINMOHSEN@cma-cgm.com>
2023-11-20 19:16:30 +0800

    Update AuditController.java

--- src/main/java/com/example/userregistration/controller/AuditController.java
+++ src/main/java/com/example/userregistration/controller/AuditController.java
@@ -6,12 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.javers.core.Changes;
 import org.javers.core.Javers;
+import org.javers.core.json.JsonConverter;
+import org.javers.core.metamodel.object.CdoSnapshot;
 import org.javers.repository.jql.QueryBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/audit")
@@ -38,6 +43,32 @@ public class AuditController {
         QueryBuilder jqlQuery = QueryBuilder.byClass(ContactEntity.class);
         Changes changes = javers.findChanges(jqlQuery.build());
         return ResponseEntity.ok().body("<pre>" + changes.prettyPrint() + "</pre>");
+    }
+
+    @GetMapping("/contact/snapshots")
+    public String getContactEntitySnapshots() {
+        QueryBuilder jqlQuery = QueryBuilder.byClass(ContactEntity.class);
+
+        List<CdoSnapshot> changes = new ArrayList(javers.findSnapshots(jqlQuery.build()));
+
+        changes.sort((o1, o2) -> -1 * o1.getCommitMetadata().getCommitDate().compareTo(o2.getCommitMetadata().getCommitDate()));
+
+        JsonConverter jsonConverter = javers.getJsonConverter();
+
+        return jsonConverter.toJson(changes);
+    }
+
+    @GetMapping("/contact/snapshots")
+    public String getBookingEntitySnapshots() {
+        QueryBuilder jqlQuery = QueryBuilder.byClass(BookingEntity.class);
+
+        List<CdoSnapshot> changes = new ArrayList(javers.findSnapshots(jqlQuery.build()));
+
+        changes.sort((o1, o2) -> -1 * o1.getCommitMetadata().getCommitDate().compareTo(o2.getCommitMetadata().getCommitDate()));
+
+        JsonConverter jsonConverter = javers.getJsonConverter();
+
+        return jsonConverter.toJson(changes);
     }
 
     @GetMapping("/contact/{id}")
