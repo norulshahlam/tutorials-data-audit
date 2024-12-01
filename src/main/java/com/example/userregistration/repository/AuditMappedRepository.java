dde901f3aca181eaa124f8a21e5cf8ec8757c12a
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-12-01 09:15:00 +0800

    Update AuditMappedRepository.java

--- src/main/java/com/example/userregistration/repository/AuditMappedRepository.java
+++ src/main/java/com/example/userregistration/repository/AuditMappedRepository.java
@@ -1,12 +1,10 @@
 package com.example.userregistration.repository;
 
 import com.example.userregistration.entity.AuditMapped;
-import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@JaversSpringDataAuditable
 public interface AuditMappedRepository extends JpaRepository<AuditMapped, Integer> {
 
 }
