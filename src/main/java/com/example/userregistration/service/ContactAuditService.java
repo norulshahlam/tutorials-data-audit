6ea5c5ed1871cbddfe518609dfb2fb0761cc50f8
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-12-10 19:54:18 +0800

    Update ContactAuditService.java

--- src/main/java/com/example/userregistration/service/ContactAuditService.java
+++ src/main/java/com/example/userregistration/service/ContactAuditService.java
@@ -6,6 +6,8 @@ import com.example.userregistration.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Slf4j
 public class ContactAuditService {
@@ -28,5 +30,7 @@ public class ContactAuditService {
 
     public void logUpdateContact(ContactEntity entity) {
         log.info("Updating contact: [{}]", entity);
+        Optional<ContactEntity> byId = contactRepository.findById(entity.getId());
+        byId.ifPresent(contactEntity -> log.info("Contact found: [{}]", contactEntity));
     }
 }
