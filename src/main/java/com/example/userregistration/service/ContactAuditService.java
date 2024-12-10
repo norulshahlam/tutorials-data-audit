812c4a1677eadb22791d9c88e2cda3fd7c1f8184
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-12-10 20:04:30 +0800

    Update ContactAuditService.java

--- src/main/java/com/example/userregistration/service/ContactAuditService.java
+++ src/main/java/com/example/userregistration/service/ContactAuditService.java
@@ -1,28 +1,33 @@
 package com.example.userregistration.service;
 
+import com.example.userregistration.entity.AuditMappedEntity;
 import com.example.userregistration.entity.ContactEntity;
 import com.example.userregistration.repository.AuditMappedRepository;
 import com.example.userregistration.repository.ContactRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 @Service
 @Slf4j
 public class ContactAuditService {
 
-    private final AuditMappedRepository auditLog;
+    private final AuditMappedRepository auditMappedRepository;
     private final ContactRepository contactRepository;
 
-    public ContactAuditService(AuditMappedRepository auditLog, ContactRepository contactRepository) {
-        this.auditLog = auditLog;
+    public ContactAuditService(AuditMappedRepository auditMappedRepository, ContactRepository contactRepository) {
+        this.auditMappedRepository = auditMappedRepository;
         this.contactRepository = contactRepository;
     }
 
     public void logCreateContact(ContactEntity entity) {
         log.info("Creating contact: [{}]", entity);
+        AuditMappedEntity mappedEntity = mapCreatedAndDeleted(entity, "CREATED");
+        auditMappedRepository.save(mappedEntity);
     }
+
 
     public void logUpdateContact(ContactEntity entity) {
         log.info("Updating contact: [{}]", entity);
@@ -32,6 +37,17 @@ public class ContactAuditService {
 
     public void logDeleteContact(ContactEntity entity) {
         log.info("Deleting contact: [{}]", entity);
+        AuditMappedEntity mappedEntity = mapCreatedAndDeleted(entity, "DELETED");
+        auditMappedRepository.save(mappedEntity);
+    }
+
+    private AuditMappedEntity mapCreatedAndDeleted(ContactEntity entity, String event) {
+        return auditMappedRepository.save(AuditMappedEntity.builder()
+                .id(Math.toIntExact(entity.getId()))
+                .type(event)
+                .commitDate(LocalDateTime.now())
+                .entity(entity.getClass().getSimpleName())
+                .build());
     }
 
 }
