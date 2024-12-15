a62cce72430fb4991f87bf9c7a3d2fd2ae8a0f09
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2024-12-15 09:38:32 +0800

    Update AuditController.java

--- src/main/java/com/example/userregistration/controller/AuditController.java
+++ src/main/java/com/example/userregistration/controller/AuditController.java
@@ -1,10 +1,11 @@
 package com.example.userregistration.controller;
 
 import com.example.userregistration.entity.AuditMappedEntity;
-import com.example.userregistration.repository.BookingRepository;
+import com.example.userregistration.repository.AuditMappedRepository;
 import com.example.userregistration.utils.Helper;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,18 +21,20 @@ import java.util.List;
 public class AuditController {
 
     private Helper helper;
-    private final BookingRepository bookingRepository;
+    private final AuditMappedRepository auditMappedRepository;
 
-    public AuditController(BookingRepository bookingRepository) {
-        this.bookingRepository = bookingRepository;
+    public AuditController(AuditMappedRepository auditMappedRepository) {
+        this.auditMappedRepository = auditMappedRepository;
     }
 
-
-    @GetMapping("/contactsPretty")
-    @Operation(summary = "Get all contact audit",
+    @GetMapping("/getAuditReport")
+    @Operation(summary = "Get contact audit report",
             description = "This endpoint will Get all contact audit")
-    public ResponseEntity<List<AuditMappedEntity>> getContractEntityChangesPrettyAll() {
-
+    public ResponseEntity<List<AuditMappedEntity>> getAuditReport() {
+        List<AuditMappedEntity> all = auditMappedRepository.findAll();
+        if (ObjectUtils.isNotEmpty(all)) {
+            helper.exportAsText(all);
+        }
         return ResponseEntity.ok(new ArrayList<>());
     }
 
