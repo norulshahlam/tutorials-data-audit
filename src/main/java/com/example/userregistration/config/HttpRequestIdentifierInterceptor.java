bbaa774c1926f04c360dfc75081da96f03cb1df0
norulshahlam <45616013+norulshahlam@users.noreply.github.com>
2025-06-13 06:57:53 +0800

    refactor

--- src/main/java/com/example/userregistration/config/HttpRequestIdentifierInterceptor.java
+++ src/main/java/com/example/userregistration/config/HttpRequestIdentifierInterceptor.java
@@ -1,5 +1,6 @@
 package com.example.userregistration.config;
 
+import lombok.NonNull;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,7 +13,10 @@ import java.util.UUID;
 @Configuration
 public class HttpRequestIdentifierInterceptor implements HandlerInterceptor, WebMvcConfigurer {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull Object handler) throws Exception {
         request.setAttribute("requestId", UUID.randomUUID().toString());
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
